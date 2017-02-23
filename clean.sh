ed23a9b0e38c09744c61027cfbd5ed7c9651b361
Laurent Nguyen <laurent.nguyen@cea.fr>
2017-02-23 23:14:18 +0100

    Add core source files

---
+++ clean.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+rm -rf *~ */*~ \
+   aclocal.m4 \
+   autom4te.cache \
+   depcomp \
+   install-sh \
+   missing \
+   configure \
+   config.h.in \
+   Makefile.in \
+   */Makefile.in
+
+
