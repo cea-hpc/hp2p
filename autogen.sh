d111e2161b3e5d351014818f75897d0a99a0d6de
Laurent Nguyen <laurent.nguyen@cea.fr>
2017-12-01 15:09:56 +0100

    shell scripts: use correct shebang
       Thank Dominique Martinet for the advices

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,3 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 autoreconf --install
--- clean.sh
+++ clean.sh
@@ -1,14 +1,17 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
-rm -rf *~ */*~ \
-   aclocal.m4 \
-   autom4te.cache \
-   depcomp \
-   install-sh \
-   missing \
-   configure \
-   config.h.in \
-   Makefile.in \
-   */Makefile.in
+# rm -rf *~ */*~ \
+#    aclocal.m4 \
+#    autom4te.cache \
+#    depcomp \
+#    install-sh \
+#    missing \
+#    configure \
+#    config.h.in \
+#    Makefile.in \
+#    */Makefile.in
+
+git clean -f -x -d
+
 
 
